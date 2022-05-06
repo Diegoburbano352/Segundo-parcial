e0552e749938429bee7a877a13659301f582e25e
DiegoBúrbano <dburbanog1@miumg.edu,gt>
2022-05-05 21:20:47 -0600

    Segundo_parcial

---
+++ src/tabla_hash/main_tabla_parcial.java
@@ -0,0 +1,19 @@
+package tabla_hash;
+
+/**
+ *
+ * @author Diego
+ */
+public class main_tabla_parcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        frm_tabla frm = new frm_tabla();
+        frm.show();
+    }
+    
+}
